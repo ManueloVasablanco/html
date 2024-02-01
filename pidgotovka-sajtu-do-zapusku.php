30efe78669287887a6a4c3f832dfaabf801c66f4
Manuelo Vasblanco <manuelovasblanco@gmail.com>
2024-02-01 02:15:41 -0800

    'f'

---
+++ pidgotovka-sajtu-do-zapusku.php
@@ -0,0 +1,260 @@
+
+<?php require 'header.php'; ?>
+<div class="big hero-wrapper animate-wrapper mb-30 white-bg">
+          <div class="container w1240">
+             <div class="hero hero-services hero-services-promotion">
+                <div class="hero-grid">
+                   <div class="item-row centered-item">
+                      <div class="h2">Подготовка сайта к запуску</div>
+                      <div class="fz20">Готовим техническое задание для разработчиков сайта в соответствии с требованиями поисковых систем для корректного ранжирования сайта в будущем.</div><a class="btn red" href="#form">Оставить заявку</a >
+                   </div>
+                   <div class="item-row centered-item">
+                      <div class="hero-img hero-img-promotion"><img class="lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="Подготовка сайта к запуску" до запуска" data-src="img/1_rus_retina.jpg"></div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="action-0-wrapper section-wrapper white-bg animate-wrapper">
+          <div class="action-list">
+             <div class="action-lis-items full-list list">
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Техническое задание</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Разработка технического задания в соответствии с требованиями поисковых систем к сайтам.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Анализ конкурентов и их ссылочный профиль</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Проводим детальный анализ конкурентов, их ссылочного профиля и стратегий, благодаря которым они вышли в ТОП поисковой выдачи.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Сбор и кластеризация ключевых слов</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Проводим сбор семантического ядра в соответствии со сферой бизнеса клиента. Распределяем собранные запросы по кластерам и показываем связки со страницами сайта.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Сбор частотности</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">В соответствии с собранным семантическим ядром снимаем частотность ключевых слов для понимания потенциала развития и продвижения проекта в поисковых системах.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Формирование структуры сайта</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Формируем рекомендуемую структуру сайта на основе семантического ядра.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Анализ ссылочного профиля</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Анализ ссылочного профиля сайта (если он уже существует) и формирование ссылочной стратегии на 3 месяца.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Составление ТС на внедрение ЦЕЛЬ или шаблонов</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Формируем шаблоны МЕТА для внедрения их на страницы сайта в соответствии со структурой.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div style="cursor: default;" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Составление отчета</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Подготовка детального отчета о выполненных работах.</div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="cost-wrapper section-wrapper white-bg animate-wrapper">
+          <div class="container w1240">
+             <div class="coast-action">
+                <div class="hero-grid">
+                   <div class="item-row">
+                      <div class="text-40">Стоимость подготовки сайта к продвижению</div>
+                   </div>
+                   <div class="item-row cost-value">
+                      <div class="p">Подготовка сайта к продвижению – это важный этап работ при разработке сайта. Она охватывает как сбор семантики и оптимизацию сайта, так и разработку структуры и написание ТС на разработку. Стоимость работ формируется в зависимости от величины проекта, сферы и рынка, на котором будет представлен проект.</div>
+                      <div class="text-40 item-price">от 539€ <span></span></div><a class="btn red" href="#form">Оставить заявку</a>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="section-wrapper seo-promotion-list white-bg animate-wrapper">
+          <div class="container w1240">
+             <div class="text-40 section-title">Прочие услуги компании</div>
+          </div>
+
+         <div class="action-list">
+             <div class="action-lis-items full-list list"><a href="seo-prosuvannya-sajtu.php" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">SEO-продвижение сайтов</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">SEO-продвижение сайта — наиболее эффективный и выгодный канал для получения клиентов из поисковых систем.</div>
+                         </div>
+                      </div>
+                   </div>
+                </a><a href="zbir-semantichnogo-yadra.php" class="list-item">
+                   <div class="container w1240">
+                      <div class="items-row type-one">
+                         <div class="item-row">
+                            <div class="fz28">Сбор семантического ядра сайта</div>
+                         </div>
+                         <div class="item-row">
+                            <div class="p">Услуги сбора семантического ядра ищут пользователи, которые уже активно занимаются SEO-продвижением своего сайта или наоборот, только задумываются по этому продвижению.</div>
+                         </div>
+                      </div>
+                   </div>
+                </a></div>
+          </div>
+       </div>
+       <div class="faq-section-wrapper animate-wrapper">
+          <div class="container w860">
+             <div class="faq-section">
+                <div class="text-40 section-title">Вопросы и ответы</div>
+                <ul class="faq">
+                   <li>
+                      <div class="faq-title">
+                         <div class="h4">От чего зависит стоимость SEO на этапе создания сайта?</div>
+                         <div class="faq-spiner"><span></span><span></span></div>
+                      </div>
+                      <div class="faq-body">Стоимость SEO-раскрутки зависит от объема посадочных страниц на вашем сайте.</div>
+                   </li>
+                   <li>
+                      <div class="faq-title">
+                         <div class="h4">Сколько времени длится подготовительный этап?</div>
+                         <div class="faq-spiner"><span></span><span></span></div>
+                      </div>
+                      <div class="faq-body">На срок таких работ влияет величина сайта, а также скорость внедрения полученных рекомендаций на сайт.</div>
+                   </li>
+                   <li>
+                      <div class="faq-title">
+                         <div class="h4">Зачем нужно SEO на этапе разработки сайта?</div>
+                         <div class="faq-spiner"><span></span><span></span></div>
+                      </div>
+                      <div class="faq-body">Для того, чтобы сайт с первого дня работы соответствовал требованиям поисковиков, и не было необходимости его глобально перерабатывать в дальнейшем. Это позволяет сэкономить деньги и создает фундамент для эффективного продвижения проекта и его роста в поисковой выдаче.</div>
+                   </li>
+                </ul>
+             </div>
+          </div>
+       </div>
+      
+      <?php require 'formular.php'; ?>
+
+
+      <div class="mb-30 section-wrapper animate-wrapper seo-section-wrapper white-bg">
+          <div class="container w1240">
+             <div class="seo-section text-content">
+                <h1>Продвижение нового сайта на этапе разработки</h1>
+                <p>SEO продвижение молодого сайта - это комплекс работ, исследований и подробного изучения рынка, что гарантирует положительный результат уже во время первого запуска. Весь контент оптимизирован под поисковые системы и не требует дополнительной доработки. Это гарантирует качественное наполнение, конкурентоспособность и попадание на первые страницы поисковиков. Потребность в этой услуге возросла с расширением и наполнением информационной базы, требующей технической обработки.</p>
+                <h3>Преимущества СЕО на этапе разработки:</h3>
+                <ul>
+                   <li>Максимальная эффективность в соответствии с основными требованиями SEO.</li>
+                   <li>Четкость мнения, структурность и правильное заполнение всех ниш.</li>
+                   <li>Качественные тексты и готовый контент, без переплат за его оптимизацию;</li>
+                   <li>Готовность сайта к работе по первому требованию;</li>
+                   <li>Экономия средств на дополнительную корректуру, оптимизацию, продвижение.</li>
+                </ul>
+                <p>Только SEO на этапе разработки гарантирует создание полноценного вебсайта с семантическим ядром, дизайном и настройками аналитики.</p>
+                <h2>Зачем необходимо SEO на этапе разработки</h2>
+                <p>Говоря о конкретных действиях, потребностях внедрения новых разработок в продвижении бизнеса, в сети предусматривается скорость выполнения запроса, прибыльность компании и ее популярность уже на первом этапе работы.</p>
+                <p>Эта область постоянно меняется, превращается и требует постоянного изучения. Поэтому продвижение нового сайта в поисковике Google — это трудоемкий и затратный процесс, который лучше доверить специалистам.</p>
+                <h3>Этапы работы профильной компании в оптимизации сайтов:</h3>
+                <ul>
+                <li>Изучение тематики будущего сайта.</li>
+                   <li>Получение информации о деятельности конкурентов и позиционировании на рынке.</li>
+                   <li>Создание семантического ядра с последующим структурным планом страниц.</li>
+                   <li>Комплексный подход к программированию и правильное распределение приоритетов.</li>
+                   <li>При наличии старого портала проводится дополнительное его изучение для исключения снижения активности и просадки трафика.</li>
+                   <li>Оптимизация каждого раздела молодого сайта и наполнение качественным контентом.</li>
+                   <li>Весь ресурс проходит проверку и предварительное испытание, позволяющее оценить конкуренцию и сделать правки к сдаче проекта.</li>
+                </ul>
+                <h2>Подготовка сайта к продвижению - этапы работ</h2>
+                <p>Нацеленность на результат и прибыльность с первого запуска — это вопросы, которые решаются с помощью профильных специалистов. Подготовка сайта к продвижению включает в себя несколько информационных этапов, а также формирование бюджета.</p>
+                <h3>Сбор информации</h3>
+                <p>Необходимо изучить схожие ресурсы, определить конкретный продукт и целевую аудиторию — непосредственного потребителя.</p>
+                <h3>Составление структуры и семантического ядра для нового сайта</h3>
+                <p>Страница получает максимальное количество просмотров, а посетитель — исчерпывающую информацию о деятельности компании.</p>
+                <h3>Техническая SEO оптимизация нового сайта</h3>
+                <p>Посылочный запрос, а также частота выдачи согласно заданному поиску — уникальность работы технического отдела нашей компании.</p>
+                <h3>Уникальный контент и SEO тексты</h3>
+                <p>Текст создается строго по тематике, отвечает всем требованиям и уникален.</p>
+                <h2>Стоимость продвижения молодого сайта в поисковиках</h2>
+                <p>Говорить о стоимости выполнения заказа можно только после полного изучения, планирования и получения целей от заказчика и объемов будущей работы. Также уменьшить затраты поможет <a href="seo-prosuvannya-sajtu.php">SEO оптимизация</a> перед запуском сайта и подробное изучение будущих сложностей профильными специалистами с опытом работы.</p>
+                <h2>Как заказать оптимизацию сайта на этапе разработки</h2>
+                <p>Эффективность сотрудничества с нами подтверждается производительностью и опытом работы. Заказ SEO для нового сайта работает на всей территории Казахстана.</p>
+                <p>На сайте действует форма для обратной связи, а также присутствуют телефоны непосредственно связи с сотрудниками компании.</p>
+             </div>
+             <div class="readmore revers seo-more" data-active="Свернуть" data-false="Читать дальше">Читать дальше</div>
+          </div>
+       </div>
+       <div class="breadcrumps-wrapper">
+          <div class="container w1240">
+             <ul class="breadcrumps">
+                <li class="fz14"><a class="fz14" href="index.php">Брама & К</a></li>
+                <li class="fz14"><a class="fz14" href="seo-prosuvannya-sajtu.php">SEO-продвижение сайтов</a></li>
+                <li class="fz14"><span>Подготовка сайта к запуску</span></li>
+             </ul>
+          </div>
+       </div>
+    </div>
+    <footer class="main-footer-wrapper">
+
+    </main>
+
+<?php require 'footer.php'; ?>
