67e563fa6c8f8c59fb3377a85afd04cd0e079d8c
Manuelo Vasblanco <manuelovasblanco@gmail.com>
2024-01-31 18:48:35 -0800

    'f'

---
+++ seo-audit-sajtiv.php
@@ -0,0 +1,213 @@
+<?php require 'header.php'; ?>
+
+<main>
+<div class="big hero-wrapper white-bg animate-wrapper">
+   <div class="container w860">
+      <div class="hero hero-services">
+         <div class="h2">SEO аудит сайтов</div>
+         <div class="fz20">Проведем детальный SEO аудит вашего сайта и дадим рекомендации по его улучшению.</div><a class="btn red" href="#form">Оставить заявку</a>
+      </div>
+   </div>
+</div>
+<div class="action-0-wrapper section-wrapper white-bg animate-wrapper">
+   <div class="action-list">
+      <div class="action-lis-items full-list list">
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Проведение полного технического аудита сайта</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">Разработка технического задания в соответствии с требованиями поисковых систем по созданию сайтов.</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Составление ТС на устранение ошибок</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">Составляем детализированное ТС для разработчиков на основании проведенного аудита.</div>
+                  </div>
+               </div>
+            </div>
+         </div>               
+         
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Анализ конкурентов и их ссылочной стратегии</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">Проводим анализ сайтов конкурентов, а также их ссылочных профилей и стратегий, которые позволили им выйти в ТОП поисковой выдачи.</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Анализ ссылочного профиля</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">Проводим анализ ссылочного профиля сайта (если есть) и разрабатываем ссылочную стратегию на 3 месяца. </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Коррекция стратегии продвижения</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">На основании полученных данных по анализу, корректируем нынешнюю стратегию для получения ТОП позиций в перспективе.</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div style="cursor: default;" class="list-item">
+            <div class="container w1240">
+               <div class="items-row type-one">
+                  <div class="item-row">
+                     <div class="fz28">Составление отчета</div>
+                  </div>
+                  <div class="item-row">
+                     <div class="p">Подготовка детального отчета о выполненных работах.</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<div class="cost-wrapper section-wrapper white-bg animate-wrapper">
+   <div class="container w1240">
+      <div class="coast-action">
+         <div class="hero-grid">
+            <div class="item-row">
+               <div class="text-40">Стоимость SEO аудита сайта</div>
+            </div>
+            <div class="item-row cost-value">
+               <div class="p">Аудит сайта — это его всесторонний анализ с целью выявления тех упущений и ошибок, которые мешают сайту выйти в ТОП. Специалисты нашей компании проверяют сайт на соответствие требованиям поисковых систем и дают рекомендации по устранению выявленных недостатков и внедрению необходимых поправок.</div>
+               <div class="text-40 item-price"><span></span></div><a class="btn red" href="#form">Оставить заявку</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<div class="faq-section-wrapper animate-wrapper">
+   <div class="container w860">
+      <div class="faq-section">
+         <div class="text-40 section-title">Вопросы и ответы</div>
+         <ul class="faq">
+            <li>
+               <div class="faq-title">
+                  <div class="h4">Зачем необходимо проводить SEO-аудит сайта?</div>
+                  <div class="faq-spiner"><span></span><span></span></div>
+               </div>
+               <div class="faq-body">Данный анализ позволяет найти все проблемы на сайте, которые мешают росту проекта в показателях трафика и конверсий. Наши специалисты подготовят документы с техническими ошибками и подробным описанием решения каждой отдельной проблемы, что повысит показатель качества сайта для поисковых систем и ваших целевых клиентов.</div>
+            </li>
+            <li>
+               <div class="faq-title">
+                  <div class="h4">Как часто нужно проводить аудит сайта?</div>
+                  <div class="faq-spiner"><span></span><span></span></div>
+               </div>
+               <div class="faq-body">SEO-аудит сайта необходимо проводить не реже одного раза в месяц для постоянного контроля вновь возникающих проблем. Наша услуга нацелена на глобальный анализ сайта для поиска всех существующих недоработок и ошибок, их разрешения и предотвращения их появления в будущем.</div>
+            </li>
+            <li>
+               <div class="faq-title">
+                  <div class="h4">От чего зависит стоимость SEO-анализа сайта?</div>
+                  <div class="faq-spiner"><span></span><span></span></div>
+               </div>
+               <div class="faq-body">Стоимость SEO-аудита сайта зависит от объема посадочных страниц на вашем сайте.</div>
+            </li>
+         </ul>
+      </div>
+   </div>
+</div>
+
+<div id="form" class="brief-services brief-wrapper section-wrapper animate-wrapper white-bg">
+         <div class="container w860 ">
+            <div class="brief">
+               <div class="h2 section-title with-more">Залиште заявку</div>
+               <form enctype="multipart/form-data" data-request="GlobalForm::onSend" data-request-validate data-request-files data-request-flash class="brief-form" data-request-success="window.dataLayer = window.dataLayer || []; window.dataLayer.push({'event': 'form_submit', 'data': $('.brief-form').serializeArray()});$('.brief-form').trigger('reset');$('.form-row').addClass('val');$('#file-upload-filename').text('');">
+                  <input type="hidden" name="lang" value="ua">
+                  <div class="h4 section-title">Контактні дані</div>
+                  <div class="brief-grid">
+                     <div class="form-row val"><input oninput="checkValue(this)" name="name" id="name" type="text"><label for="name">Ім'я та прізвище</label><span class="errors" data-validate-for="name"></span></div>
+                     <div class="form-row val"><input oninput="checkValue(this)" name="email" id="email" type="text"><label for="email">Ел. пошта</label><span class="errors" data-validate-for="email"></span></div>
+                     <div class="form-row val"><input oninput="checkValue(this)" name="tel" id="tel" type="text"><label for="tel">Телефон або месенджер</label><span class="errors" data-validate-for="tel"></span></div>
+                     <div class="form-row val"><input oninput="checkValue(this)" name="company" id="company" type="text"><label for="company">Компанія</label></div>
+                  </div>
+                  <div class="h4 section-title">Розкажіть про проєкт</div>
+                  <div class="form-row val"><input oninput="checkValue(this)" name="msg" id="msg" type="text"><label for="msg">Розкажіть про проєкт</label></div>
+                  <div class="form-action">
+                     <div class="action-buttons"><button data-attach-loading class="btn red">Відправити заявку</button><label for="file" class="btn-load"><span class="fz16">Прикріпити файл</span></label><input style="display: none" name="attachment" type="file" id="file"><span onclick="resetFile(this)" id="file-upload-filename"></span></div>
+                     <div class="accept">Натискаючи на кнопку, ви даєте згоду на <a class="policy" href="policy.php" target="_blank">обробку персональних даних і погоджуєтеся з політикою конфіденційності.</a>                     </div>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+
+
+
+      <div class="mb-30 section-wrapper animate-wrapper seo-section-wrapper white-bg">
+          <div class="container w1240">
+             <div class="seo-section text-content">
+                <h1>SEO аудит сайтов</h1>
+                <p>Сайт — инструмент бизнеса, и он должен быть не только визуально приятным и понятным для пользователя, но и оптимизированным под поисковую систему Google. Когда сайт неэффективен и не выполняет свою основную функцию — продаж, стоит задуматься о SEO аудите.</p>
+                <p>Поисковый аудит сайта производится для анализа фактической оптимизации сайта. После, составляется перечень рекомендаций, в результате выполнения которых произойдет рост позиций в поисковой выдаче, трафике и доверии клиентов.</p>
+                <p>Оставьте заявку или позвоните по телефону, и мы вас проконсультируем о необходимости и целесообразности SEO-аудита для вашего сайта.</p>
+                <h2>В каких ситуациях проводим SEO аудит сайта</h2>
+                <p>Заказать SEO-анализ стоит в двух ситуациях: когда у вас низкие показатели трафика из Google и продаж или когда есть сомнения в качестве работ текущего SEO подрядчика.</p>
+                <p>В первом случае мы анализируем состояние сайта и составляем технический отчет об аудите сайта с рекомендациями. В результате технического аудита клиент получает отчет и принимает решение о необходимости <a href="seo-prosuvannya-sajtu.php">SEO оптимизации сайта</a>.</p>
+                <p>Если клиент уже работает с подрядчиком, но недоволен расходованием бюджета, показателями, динамикой роста позиций — мы проводим аудит как независимая сторона. Наша задача — проверить качество выполненных работ и заключить работу подрядчика.</p>
+                <h2>Из чего состоит SEO аудит</h2>
+                <p>Чтобы детально изучить сайт, мы проводим анализ:</p>
+                <ul>
+                   <li>ссылочной массы сайта;</li>
+                   <li>внутренней оптимизации сайта (наличие дубликатов страниц, анализ метатегов, поиск 404 ошибок, корректность переезда на https и многое другое);</li>
+                   <li>контента на сайте (проверяем на спамность, уникальность, насколько он релевантен тем ключевым словам, по которым продвигается);</li>
+                   <li><a href="zbir-semantichnogo-yadra.php">семантического ядра</a>.</li>
+                </ul>
+                
+                <p>После проведения аудита мы составляем техническое задание по устранению технических неисправностей на сайте с точки зрения поисковых систем, а также вывод по аудиту, из которого можно составить стратегию продвижения и линкбилдинга для сайта.</p>
+                <p>Все рекомендации по СЕО оптимизации сайта предоставляем в двух файлах: .xls таблица с отчетом о ссылочном профиле сайта клиента и его конкурентах, а также .pdf файле с детализацией аудита и выводов о проделанной работе.</p>
+                <h3>Цена поискового аудита в нашей компании</h3>
+                <p>Цена услуги SEO-анализа сайта на основе задач клиента, его сайта, объема работ и сроков, в течение которых необходимо выполнить аудит.</p>
+                <h2>Почему стоит заказать технический SEO-аудит сайта у нашей компании</h2>
+                <p>Мы окунаемся в нишу вашего бизнеса, изучаем деятельность, специфику работы и стратегию продвижения конкурентов в онлайн. Такой подход более фундаментален, и позволяет не только изучить проблемы вашего сайта, но и составить эффективные рекомендации по его продвижению.</p>
+                <p>Почему стоит заказать СЕО аудит сайта у нас:</p>
+                <ul>
+                   <li>подробный аудит сайта клиента по более чем сотне параметрам;</li>
+                   <li>оптимальное время проведения аудита от 4 до 7 дней;</li>
+                   <li>возможность аудиторской проверки стороннего СЕО подрядчика;</li>
+                   <li>повышение поискового трафика, продаж и улучшение поведенческих факторов после внедрения наших рекомендаций.</li>
+                </ul>
+                <p>Хотите, чтобы ваш сайт продавал? Закажите коммерческий и комплексный SEO аудит в <a href="index.php">SEO-студии AdsUa</a>. Мы сделаем анализ более сотни параметров, которые критически важны во время SEO продвижения сайта.</p>
+             </div>
+             <div class="readmore revers seo-more" data-active="Свернуть" data-false="Читать дальше">Читать дальше</div>
+          </div>
+       </div>
+       <div class="breadcrumps-wrapper">
+          <div class="container w1240">
+             <ul class="breadcrumps">
+                <li class="fz14"><a class="fz14" href="#">Брума & К</a></li>
+                <li class="fz14"><a class="fz14" href="seo-prosuvannya-sajtu.php">SEO-продвижение сайтов</a></li>
+                <li class="fz14"><span>SEO-аудит сайтов</span></li>
+             </ul>
+          </div>
+       </div>
+      </main>
+
+      <?php require 'footer.php'; ?>
